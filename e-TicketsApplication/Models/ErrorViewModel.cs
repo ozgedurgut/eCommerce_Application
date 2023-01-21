e8fff23c06909a9abdafe4e1b2f5524c6cdd181d
Ozge Hulya Durgut <ozgehulyadurgut@gmail.com>
2023-01-21 12:32:31 +0300

    Add project files.

---
+++ e-TicketsApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace e_TicketsApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
