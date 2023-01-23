b25561a91f3a966324992b374d127bba7f26d220
Ozge Hulya Durgut <ozgehulyadurgut@gmail.com>
2023-01-23 19:00:49 +0300

    Design nav bar

--- e-TicketsApplication/Controllers/HomeController.cs
+++ e-TicketsApplication/Controllers/HomeController.cs
@@ -22,16 +22,5 @@ namespace e_TicketsApplication.Controllers
         {
             return View();
         }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
