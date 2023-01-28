a64e477767ef267757a882f6ac7de5ec24cea700
Ozge Hulya Durgut <ozgehulyadurgut@gmail.com>
2023-01-28 15:41:03 +0300

    Updating data in db

--- e-TicketsApplication/Controllers/ActorsController.cs
+++ e-TicketsApplication/Controllers/ActorsController.cs
@@ -17,7 +17,7 @@ namespace e_TicketsApplication.Controllers
         public async Task<IActionResult> Index()
         {
 
-            return View(await _service.GetAll());
+            return View(await _service.GetAllAsync());
         }
         
         public ActorsController(IActorsService service)
@@ -36,7 +36,33 @@ namespace e_TicketsApplication.Controllers
             {
                 return View(actor);
             }
-            _service.Add(actor);
+            await _service.AddAsync(actor);
+            return RedirectToAction(nameof(Index));
+        }
+
+        //Get: Actors/Details/1
+        public async Task<IActionResult> Details(int id) 
+        {
+            var actorDetails = await _service.GetByIdAsync(id);
+            if (actorDetails == null) return View("NotFound");
+            return View(actorDetails);
+        }
+
+        //Get: Actors/Edit
+        public async Task <IActionResult> Edit(int id)
+        {
+            var actorDetails = await _service.GetByIdAsync(id);
+            if (actorDetails == null) return View("NotFound");
+            return View(actorDetails);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id,[Bind("Id,Fullname,ProfilePictureURL,Bio")] Actor actor)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(actor);
+            }
+            await _service.UpdateAsync(id,actor);
             return RedirectToAction(nameof(Index));
         }
     }
--- e-TicketsApplication/Data/Services/ActorsService.cs
+++ e-TicketsApplication/Data/Services/ActorsService.cs
@@ -15,10 +15,10 @@ namespace e_TicketsApplication.Data.Services
             _context = context;
         }
 
-        public void Add(Actor actor)
+        public async Task AddAsync(Actor actor)
         {
-            _context.Actors.Add(actor);
-            _context.SaveChanges();
+            await _context.Actors.AddAsync(actor);
+            await _context.SaveChangesAsync();
         }
 
         public void Delete(int id)
@@ -26,25 +26,23 @@ namespace e_TicketsApplication.Data.Services
             throw new NotImplementedException();
         }
 
-        public async Task<IEnumerable<Actor>> GetAll()
+        public async Task<IEnumerable<Actor>> GetAllAsync()
         {
             var result = await _context.Actors.ToListAsync();
             return result;
         }
 
-        public Task GetAllAsync()
+        public async Task<Actor> GetByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            var result = await _context.Actors.FirstOrDefaultAsync(n => n.Id == id);
+            return result;
         }
 
-        public Actor GetById(int id)
+        public async Task<Actor> UpdateAsync(int id, Actor newActor)
         {
-            throw new NotImplementedException();
-        }
-
-        public Actor Update(int id, Actor newActor)
-        {
-            throw new NotImplementedException();
+            _context.Update(newActor);
+            await _context.SaveChangesAsync();
+            return newActor;
         }
     }
 }
