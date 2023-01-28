ee7c2276d2e3253a42e3456f677ac143b723ae7e
Ozge Hulya Durgut <ozgehulyadurgut@gmail.com>
2023-01-28 16:11:14 +0300

    Adding EntityBaseRepository files

---
+++ e-TicketsApplication/Data/Base/IEntityBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace e_TicketsApplication.Data.Base
+{
+    public interface IEntityBase
+    {
+        int Id { get; set; }
+
+    }
+}
